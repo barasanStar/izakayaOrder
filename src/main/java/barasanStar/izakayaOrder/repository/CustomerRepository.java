bd60458a8e232cf22210d61566a025a524f93fb8
barasan <barasanbarasan@gmail.com>
2025-06-19 13:29:48 +0900

    来店登録のロジックを整理した

--- src/main/java/barasanStar/izakayaOrder/repository/CustomerRepository.java
+++ src/main/java/barasanStar/izakayaOrder/repository/CustomerRepository.java
@@ -1,8 +1,19 @@
 package barasanStar.izakayaOrder.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import barasanStar.izakayaOrder.model.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	@Query("""
+			    SELECT c.visitGroup.id
+			    FROM Customer c
+			    WHERE c.customerToken = :customerToken
+			      AND c.visitGroup.status = 'ACTIVE'
+			""")
+	Optional<Long> findActiveVisitGroupIdByCustomerToken(@Param("customerToken") String customerToken);
 }
